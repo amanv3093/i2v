c827fe7e3b1256198bc8f516a5bb2e4bf675b7d5
aman verma <amanv3093@gmai.com>
2024-06-21 16:29:07 +0530

    update HashController

--- Test_DotNet/Controllers/HashController.cs
+++ Test_DotNet/Controllers/HashController.cs
@@ -1,35 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Test_DotNet.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class HashController : Controller
+    public class HashController : ControllerBase
     {
         [HttpGet("{firstName}")]
-        public JsonResult GetHash(string firstName)
+        public IActionResult GetHash(string firstName)
         {
-            string input = firstName;
-            byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
-            Response.Headers.Add("Access-Control-Allow-Origin", "*");
-            
-            using (var hash = SHA256.Create())
+            // Convert the input string to bytes using UTF-8 encoding
+            byte[] inputBytes = Encoding.UTF8.GetBytes(firstName);
+
+            // Compute the SHA256 hash
+            using (var sha256 = SHA256.Create())
             {
-                byte[] hashedBytes = hash.ComputeHash(inputBytes);
-                
-                // Return a HashResponse Object which contains the hashString (alphanumeric, without '-') converted from hashedBytes
-                var response = new HashResponse
-                {
-                    Hash = hashedBytes
-                };
-                return new JsonResult(response);
+                byte[] hashedBytes = sha256.ComputeHash(inputBytes);
+
+                // Convert the byte array to a lowercase hexadecimal string
+                string hashString = ByteArrayToHexString(hashedBytes);
+
+                // Return the hash string as JSON
+                return Ok(new HashResponse { Hash = hashString });
             }
         }
 
+        private string ByteArrayToHexString(byte[] bytes)
+        {
+            StringBuilder hex = new StringBuilder(bytes.Length * 2);
+            foreach (byte b in bytes)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            return hex.ToString();
+        }
     }
 
     public class HashResponse
--- Test_DotNet/Startup.cs
+++ Test_DotNet/Startup.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,10 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Test_DotNet
 {
@@ -32,15 +32,6 @@ namespace Test_DotNet
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test_DotNet", Version = "v1" });
             });
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy",
-                    builder => builder.WithOrigins("http://localhost:50702", "http://localhost:4200")
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
-            });
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,13 +39,10 @@ namespace Test_DotNet
         {
             if (env.IsDevelopment())
             {
-
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test_DotNet v1"));
             }
-            app.UseCors("CorsPolicy");
-
 
             app.UseHttpsRedirection();
 
